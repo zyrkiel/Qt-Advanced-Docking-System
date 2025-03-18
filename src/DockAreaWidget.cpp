46b8ab109b4dfff120b614ca125d870ed73f57bb
Thibault GEFFROY <tibo.geffroy@gmail.com>
2025-03-18 10:53:32 +0100

    Avoid window blink when handling OpenGL

--- src/DockAreaWidget.cpp
+++ src/DockAreaWidget.cpp
@@ -725,6 +725,10 @@ void CDockAreaWidget::setCurrentIndex(int index)
     Q_EMIT currentChanging(index);
     TabBar->setCurrentIndex(index);
 	d->ContentsLayout->setCurrentIndex(index);
+#ifdef USE_NATIVE_WIDGETS
+	// Force the widget to be native in order to avoid blinks with OpenGL widgets
+	d->ContentsLayout->currentWidget()->winId();
+#endif
 	d->ContentsLayout->currentWidget()->show();
 	// Force the repaint of the current widget because mix of OpenGL widgets
 	// and Raster widgets could have not been entirely rendered
