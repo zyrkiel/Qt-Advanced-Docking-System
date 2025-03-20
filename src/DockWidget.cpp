c17f4622b3344dab46bebc2ce1200bcb8b04ca95
Thibault GEFFROY <tibo.geffroy@gmail.com>
2025-03-20 12:15:48 +0100

    Fix non OpenGL CDockWidgets vanish when docking OpenGl widget in the same area
    This fix concerns only Windows behaviors

--- src/DockWidget.cpp
+++ src/DockWidget.cpp
@@ -376,6 +376,11 @@ CDockWidget::CDockWidget(CDockManager *manager, const QString &title, QWidget* p
 	: QFrame(parent),
 	  d(new DockWidgetPrivate(this))
 {
+#ifdef USE_NATIVE_WIDGETS
+	// Force the new CDockWidget to be native so OpenGL widgets are well handled
+	// with non OpenGL widgets
+	winId();
+#endif
 	d->DockManager = manager;
 	d->Layout = new QBoxLayout(QBoxLayout::TopToBottom);
 	d->Layout->setContentsMargins(0, 0, 0, 0);
